55b5dd2d10d5df429318bedc20e537f94d2b26e4
Sergiu Neamtu <sergiuneamtu29@gmail.com>
2021-10-13 07:43:42 +0000

    "feat/1__at__2021-10-13__07:43:42"

---
+++ tools/train.py
@@ -0,0 +1,44 @@
+import os
+import uuid
+
+import subprocess
+import datetime
+import git.exc
+from git import Repo
+
+import time
+st = time.time()
+backup_remote_path = r"git@github.com:nfsergiu/mytestrepo2.git"
+backup_remote_name = "backup"
+
+cur_file_path = os.path.realpath(__file__)
+repo_path = os.path.dirname(os.path.dirname(cur_file_path))
+
+repo = Repo(repo_path)
+
+try:
+    remote_backup = repo.create_remote(backup_remote_name, backup_remote_path)
+except git.exc.GitCommandError:
+    pass
+
+active_branch = repo.active_branch
+repo.remote
+
+backup_branch_name = uuid.uuid4().hex
+
+cur_time = f"{datetime.datetime.now()}".replace(" ", "__").split(".")[0]
+
+commands = [f"git checkout -b {backup_branch_name}",
+f"git add .",
+f"git commit -m \"{active_branch}__at__{cur_time}\"",
+f"git push {backup_remote_name} {backup_branch_name}",
+f"git checkout --detach",
+f"git reset --soft {active_branch}",
+f"git checkout {active_branch}",
+f"git push {backup_remote_name} refs/remotes/origin/*:refs/heads/*"
+            ]
+for command in commands:
+    print(command)
+    subprocess.run(command.split(" "))
+
+print(time.time() - st)
